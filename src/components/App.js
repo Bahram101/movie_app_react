e1584395ea90f5a6add7c9d016830c2e52bd257b
bahram <bahram101@mail.ru>
2021-06-18 16:03:42 +0600

    real api

--- src/components/App.js
+++ src/components/App.js
@@ -1,61 +1,53 @@
 import React from 'react'
 import SearchBar from './Searchbar';
 import MovieList from './MovieList';
-
-
-
-
+import axios from 'axios';
 
 
 class App extends React.Component {
 
     state = {
-        movies : [
-            {
-                "id": 7,
-                "name": "The Matrix 3",
-                "rating": "8.1",
-                "overview": "Set in the 22nd century, The Matrix tells the story of a computer hacker who joins a group of underground insurgents fighting the vast and powerful computers who now rule the earth.",
-                "imageURL": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/dXNAPwY7VrqMAo51EKhhCJfaGb5.jpg",
-                
-            },
-            {
-                "id": 8,
-                "name": "The Matrix Reloaded",
-                "rating": "6.9",
-                "imageURL": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/jBegA6V243J6HUnpcOILsRvBnGb.jpg",
-                "overview": "Set in the 22nd century, The Matrix tells the story of a computer hacker who joins a group of underground insurgents fighting the vast and powerful computers who now rule the earth.",
-                
-            },
-            {
-                "id": 11,
-                "name": "Saw 3D",
-                "rating": "7.5",
-                "overview": "Set in the 22nd century, The Matrix tells the story of a computer hacker who joins a group of underground insurgents fighting the vast and powerful computers who now rule the earth.",
-                "imageURL": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/qHCZ6LjtmqWDfXXN28TlIC9OppK.jpg",
-                
-            },
-            {
-                "id": 12,
-                "name": "Blitz 007",
-                "rating": "11",
-                "overview": "Set in the 22nd century, The Matrix tells the story of a computer hacker who joins a group of underground insurgents fighting the vast and powerful computers who now rule the earth.",
-                "imageURL": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/qCPMjT8Ld8tvs1zs7LY2jpKlRIK.jpg",
-                
-            },
-            {
-                "id": 13,
-                "name": "Hostage",
-                "rating": "6.3",
-                "imageURL": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/4hne3v6jN4MlCnhSkxOW7YspJhr.jpg",
-                "overview": "Set in the 22nd century, The Matrix tells the story of a computer hacker who joins a group of underground insurgents fighting the vast and powerful computers who now rule the earth.",
-                
-            }
-        ],        
+        movies : [],        
         searchQuery: ""
     }
 
-    deleteMovie = (movie) =>{
+    // async componentDidMount(){
+    //     const response = await fetch("http://localhost:3007/movies");
+    //     const data = await response.json()
+    //     this.setState({movies:data})
+    // }
+
+    async componentDidMount(){
+        const response = await axios.get("http://localhost:3007/movies");        
+        this.setState({movies:response.data})
+    }
+
+    // deleteMovie = (movie) =>{
+    //     const newMovieList = this.state.movies.filter(
+    //         m => m.id !== movie.id
+    //     )
+    //     this.setState({
+    //         movies:newMovieList
+    //     })
+    // }
+
+    //FETCH API    
+    // deleteMovie = async (movie) =>{
+    //     const baseUrl = `http://localhost:3007/movies/${movie.id}`
+    //     await fetch(baseUrl, {
+    //         method:"DELETE"
+    //     })
+    //     const newMovieList = this.state.movies.filter(
+    //         m => m.id !== movie.id
+    //     )
+    //     this.setState({
+    //         movies:newMovieList
+    //     })
+    // }
+
+    //AXIOS API    
+    deleteMovie = async (movie) =>{
+        axios.delete(`http://localhost:3007/movies/${movie.id}`) 
         const newMovieList = this.state.movies.filter(
             m => m.id !== movie.id
         )
